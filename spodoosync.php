1b4205996aba94d9f0158b2f54c0cab9bf13d242
Jaap Jansma <info@edeveloper.nl>
2014-07-09 17:23:31 +0200

    initial commit

---
+++ spodoosync.php
@@ -0,0 +1,150 @@
+<?php
+
+require_once 'spodoosync.civix.php';
+
+/**
+ * Implementation of hook_civicrm_odoo_alter
+ * 
+ * Through this hook we will make sure that every contact created in Odoo is a company (even if it is an individual)
+ * 
+ * @param xmlrpcval $parameters
+ * @param type $entity
+ * @param type $entity_id
+ * @param type $action
+ */
+function spodoosync_civicrm_odoo_alter_parameters(&$parameters, $entity, $entity_id, $action) {
+    if ($entity == 'civicrm_contact') {
+        $contact = civicrm_api3('Contact', 'getsingle', array('id' => $entity_id));
+        if ($contact['contact_type'] == 'Individual') {
+            $parameters['is_company'] = new xmlrpcval(true, 'boolean');
+        }
+    }
+}
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function spodoosync_civicrm_config(&$config) {
+  _spodoosync_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function spodoosync_civicrm_xmlMenu(&$files) {
+  _spodoosync_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function spodoosync_civicrm_install() {
+  $error = false;
+  $isOdooSyncInstalled = false;
+  try {
+    $extensions = civicrm_api3('Extension', 'get');  
+    foreach($extensions['values'] as $ext) {
+      if ($ext['status'] == 'installed') {
+        switch ($ext['key']) {
+          case 'org.civicoop.odoosync':
+            $isOdooSyncInstalled = true;
+            break;
+        }
+      }
+    }    
+  } catch (Exception $e) {
+    $error = true;
+  }
+  
+  
+  if ($error || !$isOdooSyncInstalled) {
+    throw new Exception('This extension requires org.civicoop.odoosync');
+  }
+  
+  
+  return _spodoosync_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function spodoosync_civicrm_uninstall() {
+  return _spodoosync_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function spodoosync_civicrm_enable() {
+  return _spodoosync_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function spodoosync_civicrm_disable() {
+  return _spodoosync_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function spodoosync_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _spodoosync_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function spodoosync_civicrm_managed(&$entities) {
+  return _spodoosync_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function spodoosync_civicrm_caseTypes(&$caseTypes) {
+  _spodoosync_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function spodoosync_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _spodoosync_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
