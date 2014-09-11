4d1a18ec6627b1ce3a1ff21d40e6bb0c26fe8c72
Jaap Jansma <info@edeveloper.nl>
2014-09-11 12:30:30 +0200

    used labels instead of name

--- CRM/Spodoosync/Synchronisator/ContactSynchronisator.php
+++ CRM/Spodoosync/Synchronisator/ContactSynchronisator.php
@@ -57,9 +57,19 @@ class CRM_Spodoosync_Synchronisator_ContactSynchronisator extends CRM_OdooContac
   protected function findProvincie($contact) {
     $search = array('sp-provincie', 'sp provincie');
     $replace = array('');
-    $name = 'SP Statenfractie '.trim(str_ireplace($search, $replace, $contact['display_name']));
+    $name = str_ireplace($search, $replace, $contact['display_name']);
     
-    return $this->findByName($name);
+    //find labels for sp staten fractie
+    $label_ids = array();
+    $parent_id = CRM_OdooContactSync_Helper_FindLabel::findLabel('SP-relaties');
+    if ($parent_id) {
+      $staten_label = CRM_OdooContactSync_Helper_FindLabel::findLabel('SP Statenfracties', $parent_id);
+      if ($staten_label) {
+        $label_ids[] = $staten_label;
+      }
+    }
+    
+    return $this->findByName($name, $label_ids);
   }
   
   /**
@@ -74,18 +84,50 @@ class CRM_Spodoosync_Synchronisator_ContactSynchronisator extends CRM_OdooContac
   protected function findAfdeling($contact) {
     $search = array('sp-afdeling', 'sp afdeling', 'afdeling');
     $replace = array('');
-    $name = 'SP Afd '.trim(str_ireplace($search, $replace, $contact['display_name']));
+    $name = str_ireplace($search, $replace, $contact['display_name']);
     
-    return $this->findByName($name);
+    //find labels for in opricht en actiefe afdeling
+    $label_ids = array();
+    $parent_id = CRM_OdooContactSync_Helper_FindLabel::findLabel('SP-relaties');
+    if ($parent_id) {
+      $afdeling_label = CRM_OdooContactSync_Helper_FindLabel::findLabel('SP afdelingen', $parent_id);
+      if ($afdeling_label) {
+        $label_ids[] = $afdeling_label;
+        $actief = CRM_OdooContactSync_Helper_FindLabel::findLabel('SP afdelingen actief', $afdeling_label);
+        if ($actief) {
+          $label_ids[] = $actief;
+        }
+        $io = CRM_OdooContactSync_Helper_FindLabel::findLabel('SP afdelingen in oprichting', $afdeling_label);
+        if ($io) {
+          $label_ids[] = $io;
+        }
+      }
+    }
+    
+    return $this->findByName($name, $label_ids);
   }
   
-  protected function findByName($name) {
+  protected function findByName($name, $label_ids = array()) {
     $key = array(
     new xmlrpcval(array(
       new xmlrpcval('name', 'string'),
       new xmlrpcval('like', 'string'),
-      new xmlrpcval($name, 'string'),
+      new xmlrpcval('%'.trim($name).'%', 'string'),
     ), "array"));
+    
+    if (is_array($label_ids) && count($label_ids)) {
+      $label_ids_rpc = array();
+      foreach($label_ids as $label_id) {
+        if ($label_id) {
+          $label_ids_rpc[] = new xmlrpcval($label_id, 'int');
+        }
+      }
+      $key[] = new xmlrpcval(array(
+        new xmlrpcval('category_id', 'string'),
+        new xmlrpcval('in', 'string'),
+        new xmlrpcval($label_ids_rpc, 'array'),
+      ), "array");
+    }
 
     $result = $this->connector->search($this->getOdooResourceType(), $key);
 
@@ -95,6 +137,8 @@ class CRM_Spodoosync_Synchronisator_ContactSynchronisator extends CRM_OdooContac
     
     return false;
   }
+  
+  
   
 }
 
