47bc61ce9a86d9e7fa818593cc52a65197bc0395
Jaap Jansma <info@edeveloper.nl>
2014-10-10 14:54:07 +0200

    update upgrader

--- CRM/Spodoosync/Upgrader.php
+++ CRM/Spodoosync/Upgrader.php
@@ -19,5 +19,19 @@ class CRM_Spodoosync_Upgrader extends CRM_Spodoosync_Upgrader_Base {
     $this->addOptionValue('sp_acceptgiro', 'Acceptgiro (Beschikbaar in Odoo)', $pi_gid);
     $this->addOptionValue('sp_automatischincasse', 'Automatisch incasso (Beschikbaar in Odoo)', $pi_gid);
   }
+  
+  protected function addOptionValue($name, $label, $option_group_id) {
+    try {
+      $exist_id = civicrm_api3('OptionValue', 'getvalue', array('return' => 'id', 'name' => $name, 'option_group_id' => $option_group_id));
+      return; //aleardy exist
+    } catch (Exception $e) {
+      //do nothing
+    }
+    
+    $params['name'] = $name;
+    $params['label'] = $label;
+    $params['option_group_id'] = $option_group_id;
+    civicrm_api3('OptionValue','create', $params);
+  }
 
 }
