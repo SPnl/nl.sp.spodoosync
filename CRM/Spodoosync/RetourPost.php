330d027267aabec870cf1e6179a99db913c4aff7
Kevin Levie <kevin@levity.nl>
2015-01-29 21:54:28 +0100

    fixed typo and changed field from opt_out to retourpost

--- CRM/Spodoosync/RetourPost.php
+++ CRM/Spodoosync/RetourPost.php
@@ -16,7 +16,7 @@ class CRM_Spodoosync_RetourPost {
     protected function __construct() {
         try {
             $this->migratie_set = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'Migratie_Contacten'));
-            $this->migratie_field = civicrm_api3('CustomField', 'getsingle', array('custom_group_id' => $this->migratie_set['id'], 'name' => 'Retourpost'));
+            $this->retourpost_field = civicrm_api3('CustomField', 'getsingle', array('custom_group_id' => $this->migratie_set['id'], 'name' => 'Retourpost'));
         } catch (Exception $e) {
             //do nothing
         }
@@ -34,14 +34,16 @@ class CRM_Spodoosync_RetourPost {
 
     public static function syncRetourPostToOdoo($contact_id, &$parameters) {
         $i = CRM_Spodoosync_RetourPost::singleton();
-        if (!is_array($i->migratie_set )|| !is_array($i->retourpost_field)) {
+
+        if (!is_array($i->migratie_set) || !is_array($i->retourpost_field)) {
             return;
         }
 
         try {
             $retourPost = civicrm_api3('Contact', 'getvalue', array('id' => $contact_id, 'return' => 'custom_'.$i->retourpost_field['id']));
-            $parameters['opt_out'] = new xmlrpcval($retourPost, 'bool');
+            $parameters['retourpost'] = new xmlrpcval($retourPost, 'boolean');
         } catch (Exception $e) {
+
             return;
         }
     }
